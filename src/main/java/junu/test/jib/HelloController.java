7c4ef383cecd6cdfc00d14f95aeeb09a6fc9b8d2
최준우 <junwoochoi@zuminternet.com>
2020-01-13 13:20:11 +0900

    feat : HelloController 구현

--- src/main/java/junu/test/jib/HelloController.java
+++ src/main/java/junu/test/jib/HelloController.java
@@ -1,5 +1,19 @@
 package junu.test.jib;
 
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toMap;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/hello")
+    public ResponseEntity hello() {
+        return ResponseEntity.ok(Stream.of("hello").collect(toMap(key -> key, val -> "world")));
+    }
 }
